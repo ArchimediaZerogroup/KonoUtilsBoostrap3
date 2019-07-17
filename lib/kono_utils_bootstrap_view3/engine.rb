47e81d77423e9e35a17b72f56933bd2a37708c39
Marino Bonetti <marinobonetti@gmail.com>
2019-07-17 14:12:08 +0200

    estrapolazione ed importazione viste per old

--- lib/kono_utils_bootstrap_view3/engine.rb
+++ lib/kono_utils_bootstrap_view3/engine.rb
@@ -5,5 +5,9 @@ module KonoUtilsBootstrapView3
     require 'bootstrap3-datetimepicker-rails'
     require 'formtastic-bootstrap'
 
+
+    initializer 'kono_utils_bootstrap_view.append_views', :before => 'kono_utils.append_views', :group => :all do |app|
+      ActionController::Base.append_view_path KonoUtilsBootstrapView3::Engine.root.join("app", "views", "kono_utils")
+    end
   end
 end
