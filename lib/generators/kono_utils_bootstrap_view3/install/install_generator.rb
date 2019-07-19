bd9f2153926587a4330eb55a9b872f439e61e002
Marino Bonetti <marinobonetti@gmail.com>
2019-07-19 09:58:57 +0200

    modifiche al generatore

--- lib/generators/kono_utils_bootstrap_view3/install/install_generator.rb
+++ lib/generators/kono_utils_bootstrap_view3/install/install_generator.rb
@@ -11,6 +11,25 @@ module KonoUtilsBootstrapView3
       run "rails g kono_utils:install"
     end
 
+    def append_dependecy_to_assets
+      requirements = [
+        'jquery/dist/jquery',
+        'kono_utils_bootstrap_view3/application'
+      ]
+      inject_into_file 'app/assets/javascripts/application.js',
+                       "#{requirements.collect {|c| "\n//= require #{c}"}.join}\n",
+                       before: "//= require_tree ."
+
+
+      requirements=[
+        'kono_utils_bootstrap_view3/application'
+      ]
+      inject_into_file 'app/assets/stylesheets/application.css',
+                       "#{requirements.collect {|c| "\n *= require #{c}"}.join}\n",
+                       before: " *= require_tree ."
+
+    end
+
     def install_node_dependency
       yarn_packages = ["patternfly-bootstrap-treeview", "moment"]
       run "yarn add #{yarn_packages.join(' ')}"
